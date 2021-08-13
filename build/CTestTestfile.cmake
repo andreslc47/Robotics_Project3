58a73aa1c8164003779610637d60acb52ae83850
Andres Leiva <andreslc47@hotmail.com>
2021-08-12 22:36:55 -0700

    My Project3, first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Desktop/Robotics_Project3/src
+# Build directory: /home/robond/Desktop/Robotics_Project3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard)
